cecfd498aa02442b74b901928e0a3e0f731965a0
Mr.Pratchayanan <Yanperm@users.noreply.github.com>
2022-06-20 22:50:25 +0700

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -12,10 +12,10 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com"> 
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
-<link href="https://fonts.googleapis.com/css2?family=Kodchasan:wght@200&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Kanit:wght@100&display=swap" rel="stylesheet">
 	<style>
 		body {
-      font-family: 'Kodchasan', sans-serif;
+      font-family: 'Kanit', sans-serif;
   background-image: url("../images/bg.jpg");
   background-size: cover;
   background-repeat: no-repeat;
