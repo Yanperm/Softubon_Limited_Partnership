4fbb77de817ab154826ddf81f82fcdb108074d33
Mr.Pratchayanan <Yanperm@users.noreply.github.com>
2022-06-22 12:10:56 +0700

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -142,7 +142,7 @@ footer {
 <body class="min-vh-100 d-flex flex-column">
     <main class="my-auto">
         <img src="<?php echo base_url(); ?>assets/coming-soon.jpeg" alt="Coming soon" class="header-img img-fluid">
-        <h1 class="page-title">We are launching soon.</h1>
+        <h1 class="page-title">ห้างหุ้นส่วนจำกัด ซอฟต์อุบล</h1>
         <p class="page-description">สินค้าไอที,พัฒนาซอฟต์แวร์,ประกันภัยทุกประเภท</p>
         <form class="form-inline mail-capture-form" action="<?php echo base_url('app/mail'); ?>" method="POST">
             <div class="form-group">
